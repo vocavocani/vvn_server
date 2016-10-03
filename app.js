9de146a23e6e59fa496fef651c537c6034f3e3f3
Nayak <wjdduq914@gmail.com>
2016-10-04 01:31:04 +0900

    [Nayak] vvn server start

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Server Port Set
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.info('[VVN Server] Application Listening on Port 3000');
+});
+
+module.exports = app;
