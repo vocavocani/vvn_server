3ddaf6659326c90d12b2c475f741bbe726d71b88
Nayak <wjdduq914@gmail.com>
2016-11-09 15:46:58 +0900

    Refactoring: Team controller callback => async/await

--- controllers/TeamCtrl.js
+++ controllers/TeamCtrl.js
@@ -5,26 +5,30 @@ const teamModel = require('../models/TeamModel');
 /*******************
  *  Create
  ********************/
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
   if (!req.body.name || !req.body.max_cap || !req.body.category || !req.body.rule || !req.body.public) {  // parameter check
     return next(9401);
   } else {
-    const team_data = {
-      user_idx: req.user_idx,
-      team_name: req.body.name,
-      team_rule: req.body.rule,
-      team_category_idx: req.body.category,
-      team_max_cap: req.body.max_cap,
-      team_public: req.body.public
-    };
+    let result = '';
 
-    teamModel.create(team_data, (err, result) =>{
-      if (err) {
-        return next(err);
-      } else {
-        return res.json(result);
-      }
-    });
+    try {
+      const team_data = {
+        user_idx: req.user_idx,
+        team_name: req.body.name,
+        team_rule: req.body.rule,
+        team_category_idx: req.body.category,
+        team_max_cap: req.body.max_cap,
+        team_public: req.body.public
+      };
+
+      result = await teamModel.create(team_data);
+
+    } catch(error) {
+      return next(error);
+    }
+
+    // success
+    return res.json(result);
   }
 };
 
@@ -35,26 +39,26 @@ exports.apply = async (req, res, next) => {
   if (!req.params.team_idx) {  // parameter check
     return next(9401);
   } else {
-    // TEAM Checking
+    let result = '';
+
     try {
+      // TEAM Checking
       await teamModel.check(req.params.team_idx);
+
+      // TEAM apply
+      const apply_data = {
+        team_idx: req.params.team_idx,
+        user_idx: req.user_idx,
+        team_member_apply_msg: req.body.message
+      };
+
+      result = await teamModel.apply(apply_data);
+
     } catch(error) {
       return next(error);
     }
 
-    // TEAM apply
-    const apply_data = {
-      team_idx: req.params.team_idx,
-      user_idx: req.user_idx,
-      team_member_apply_msg: req.body.message
-    };
-
-    teamModel.apply(apply_data, (err, result) => {
-      if (err) {
-        return next(err);
-      } else {
-        return res.json(result);
-      }
-    });
+    // success
+    return res.json(result);
   }
 };
