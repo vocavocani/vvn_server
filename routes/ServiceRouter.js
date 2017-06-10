4f2da86fa7918bd8f9beeef3c73f1912d19eb888
nayak <wjdduq914@gmail.com>
2017-06-10 21:38:29 +0900

    Create. My Team List

--- routes/ServiceRouter.js
+++ routes/ServiceRouter.js
@@ -22,16 +22,17 @@ module.exports = (router) => {
 
   // TEAM
   router.route('/team')
+    .get(AuthCtrl.auth, TeamCtrl.list)
     .post(AuthCtrl.auth, validate(ParamValidation.team_create), TeamCtrl.create);
   router.route('/team/:team_idx')
-    .get(validate(ParamValidation.team_retrieve), TeamCtrl.retrieve); // 팀 메인페이지 조회
+    .get(AuthCtrl.auth, validate(ParamValidation.team_retrieve), TeamCtrl.retrieve); // 팀 메인페이지 조회
   router.route('/team/:team_idx/apply')
     .post(AuthCtrl.auth, validate(ParamValidation.team_apply), TeamCtrl.apply)
     .put(AuthCtrl.auth, validate(ParamValidation.team_confirm), TeamCtrl.confirm);
 
   // POST
   router.route('/team/:team_idx/post')
-    .get(validate(ParamValidation.post_retrieve), PostCtrl.retrieve) // 담벼락 조회
+    .get(AuthCtrl.auth, validate(ParamValidation.post_retrieve), PostCtrl.retrieve) // 담벼락 조회
     .post(AuthCtrl.auth, validate(ParamValidation.post_write), PostCtrl.write); // 담벼락 쓰기
   router.route('/team/:team_idx/post/:post_idx')
     .put(AuthCtrl.auth, validate(ParamValidation.post_edit), PostCtrl.edit) //담벼락 수정
